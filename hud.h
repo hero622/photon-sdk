3fadd874d60ac1fe99858adcaac956b45fa01eb2
Zyntex <komocsinlaci@gmail.com>
2023-10-16 21:51:07 +0200

    change static functions to virtual

--- hud.h
+++ hud.h
@@ -22,8 +22,8 @@ namespace wh_api {
 
 class c_huds {
 private:
-	static std::vector<wh_api::c_hud *> &get_huds();
-	static std::vector<wh_api::c_thud *> &get_thuds();
+	virtual std::vector<wh_api::c_hud *> &get_huds();
+	virtual std::vector<wh_api::c_thud *> &get_thuds();
 
 public:
 	virtual void reg(wh_api::c_hud *hud);

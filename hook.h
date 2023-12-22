262a347432c67bbc742a36927beb89fd6ba45dc7
Zyntex <komocsinlaci@gmail.com>
2023-12-22 13:51:06 +0100

    value changed return on checkbox, return success on hooks

--- hook.h
+++ hook.h
@@ -5,23 +5,23 @@
 
 class c_hook {
 public:
-	virtual int create_hook( void *src, void *dst, void *&hook, void **orig );
-	virtual int remove_hook( void *hook );
-	virtual int enable_hook( void *hook );
-	virtual int disable_hook( void *hook );
+	virtual bool create_hook( void *src, void *dst, void *&hook, void **orig );
+	virtual bool remove_hook( void *hook );
+	virtual bool enable_hook( void *hook );
+	virtual bool disable_hook( void *hook );
 };
 
 #define hk_virtual( vtable, name, off ) \
-	photon->hook->create_hook( reinterpret_cast<void *>( utils::memory::get_virtual<off>( vtable ) ), reinterpret_cast<void *>( name##_hk_fn ), name##_hk, reinterpret_cast<void **>( &name ) );
+	photon->hook->create_hook( reinterpret_cast<void *>( utils::memory::get_virtual<off>( vtable ) ), reinterpret_cast<void *>( name##_hk_fn ), name##_hk, reinterpret_cast<void **>( &name ) )
 
 #define hk_inline( name, addr ) \
-	photon->hook->create_hook( reinterpret_cast<void *>( addr ), reinterpret_cast<void *>( name##_hk_fn ), name##_hk, reinterpret_cast<void **>( &name ) );
+	photon->hook->create_hook( reinterpret_cast<void *>( addr ), reinterpret_cast<void *>( name##_hk_fn ), name##_hk, reinterpret_cast<void **>( &name ) )
 
 #define unhk( name ) \
 	photon->hook->remove_hook( name##_hk )
 
 #define hk_cmd( name ) \
-	photon->con->hook_cmd( #name, name##_hk, name );
+	photon->con->hook_cmd( #name, name##_hk, name )
 
 #define unhk_cmd( name ) \
-	photon->con->unhook_cmd( #name, name );
+	photon->con->unhook_cmd( #name, name )
--- menu.h
+++ menu.h
@@ -7,7 +7,7 @@
 class c_menu {
 public:
 	virtual bool button( sdk::vec2_t size, const char *label );                                                    // Returns true on button click.
-	virtual void checkbox( bool &val, const char *label );                                                         // Checkbox using boolean value.
+	virtual bool checkbox( bool &val, const char *label );                                                         // Checkbox using boolean value.
 	virtual void slider( int &val, int min, int max, const char *label );                                          // Slider using integer values.
 	virtual void sliderf( float &val, float min, float max, const char *label );                                   // Slider using float values.
 	virtual void colorpicker( sdk::color_t &val, const char *label );                                              // Colorpicker using a 4 value datatype (R, G, B, A).
